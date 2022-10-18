70b5ba8d2f9de18952b94103fba575f9eec6c391
Yoann Bertuol <exdarface@gmail.com>
2022-10-18 18:15:00 +0200

    pipe working

--- FRONTEND/src/app/phone-pipe.pipe.ts
+++ FRONTEND/src/app/phone-pipe.pipe.ts
@@ -7,15 +7,15 @@ export class PhonePipePipe implements PipeTransform {
   transform(number: String): String {
     return (
       '+33 ' +
-      number.substring(1, 1) +
+      number.substring(1, 2) +
       '.' +
-      number.substring(2, 2) +
+      number.substring(2, 4) +
       '.' +
-      number.substring(4, 2) +
+      number.substring(4, 6) +
       '.' +
-      number.substring(6, 2) +
+      number.substring(6, 8) +
       '.' +
-      number.substring(8, 2)
+      number.substring(8, 10)
     );
   }
 }
